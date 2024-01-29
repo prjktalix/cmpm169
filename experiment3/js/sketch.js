d47f96a70959b2f095bab295adad5cb58c12adbf
prjktalix <briancamilo0720@gmail.com>
2024-01-28 19:15:40 -0800

    added exp3

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -24,6 +24,16 @@ class MyClass {
         // code to run when method is called
     }
 }
+let x = [];
+let y = [];
+let u = 20;
+let s = 3;
+let r = 200;
+let xOld = [];
+let yOld = [];
+let m, n, v;
+let trailLength = 10;
+let color = 0;
 
 // setup() function is called once when the program starts
 function setup() {
@@ -41,6 +51,20 @@ function setup() {
 
     var centerHorz = windowWidth / 2;
     var centerVert = windowHeight / 2;
+
+    colorMode(HSB, 360, 100, 100, 100);
+  m = width / 2;
+  n = height / 2;
+  v = radians(360 / u);
+  for (let i = 0; i < u; i++) {
+    x[i] = cos(v * i) * r;
+    y[i] = sin(v * i) * r;
+    xOld[i] = new Array(trailLength).fill(x[i]);
+    yOld[i] = new Array(trailLength).fill(y[i]);
+  }
+  strokeWeight(1);
+  background(0);
+  frameRate(30);
 }
 
 // draw() function is called repeatedly, it's the main animation loop
@@ -61,7 +85,40 @@ function draw() {
     text("p5*", centerHorz + 10, centerVert + 200);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
+function draw() {
+    background(0, 15);
+    color = (color + 1) % 360;
+    
+    for (let i = 0; i < u; i++) {
+      
+      x[i] += random(-s, s);
+      y[i] += random(-s, s);
+      
+      // update history and draw trail lines
+      xOld[i].unshift(x[i]);
+      yOld[i].unshift(y[i]);
+      if (xOld[i].length > trailLength) xOld[i].pop();
+      if (yOld[i].length > trailLength) yOld[i].pop();
+      
+      for (let j = 0; j < xOld[i].length - 1; j++) {
+        stroke((color + i * 18) % 360, 80, 90, map(j, 0, xOld[i].length - 1, 100, 0));
+        line(xOld[i][j], yOld[i][j], xOld[i][j + 1], yOld[i][j + 1]);
+      }
+      
+      fill((color + i * 18) % 360, 80, 90);
+      let dynamicRadius = map(dist(mouseX, mouseY, m, n), 0, width, 10, r);
+      ellipse(x[i], y[i], dynamicRadius * 0.1, dynamicRadius * 0.1);
+    }
+  }
+  
+  function mousePressed() {
+    m = mouseX;
+    n = mouseY;
+    r = random(100, 300);
+    for (let i = 0; i < u; i++) {
+      x[i] = cos(v * i) * r + m;
+      y[i] = sin(v * i) * r + n;
+      xOld[i] = new Array(trailLength).fill(x[i]);
+      yOld[i] = new Array(trailLength).fill(y[i]);
+    }
+  }
